830eb856f1cbf335b9b5fa50bd9e15144020bed7
Vydr'Oz <61025448+VydrOz@users.noreply.github.com>
2022-02-14 21:59:08 +0100

    Create RenamingFilesCorrectly.ps1

---
+++ RenamingFilesCorrectly.ps1
@@ -0,0 +1,55 @@
+[cultureinfo]::CurrentUICulture = 'en-US'
+
+#variables
+$hashPreview = @{}
+$e = [char]27
+$specialChars = '-#${}()[]+' 		# list of characters to remove
+$extension = ".txt"         		# file type to rename
+
+# functions
+function SerializeString {
+    param ([string]$str, [string]$ext)
+    $rePattern = ($specialChars.ToCharArray() |ForEach-Object { [regex]::Escape($_) }) -join "|"
+    $str = $str -replace $rePattern,''      # remove special characters
+    $str = $str.Trim() -replace '\s+', '_'  # remove first and last spaces & replace spaces by "_"
+    "${str}${ext}"
+}
+Write-Host "Type of file retrieved : " -NoNewLine
+Write-Host -ForegroundColor Blue $extension
+Write-Host "Special characters will being removed : " -NoNewLine
+Write-Host -ForegroundColor Blue $specialChars
+Write-Host ""
+
+# start preview
+Write-Host -ForegroundColor Green "Overview of changes"
+$files = Get-ChildItem -Path $PSScriptRoot -Filter "*${extension}" |
+    Foreach-Object {
+        $fileName = [System.IO.Path]::GetFileNameWithoutExtension($_.Name)
+        $newFileName = SerializeString $fileName $extension
+        $hashPreview.Add($_.Name, $newFileName)
+    }
+	
+# show preview
+$hashPreview | Format-Table @{L='Before';E={"$e[0m$($_.Name)${e}[0m"}; width = 60}, @{L='After';E={"$e[93m$($_.Value)${e}[0m"}; width = 60}
+
+# confirm modifications Y/N
+$msg     = 'Are you sure you want to rename these files?
+    '
+$options = '&Yes', '&No'
+$default = 1  # 0=Yes, 1=No
+$response = $Host.UI.PromptForChoice('', $msg, $options, $default)
+
+if ($response -eq 0) { #Yes ? start rename process
+    $files = Get-ChildItem -Path $PSScriptRoot -Filter "*${extension}" |
+    Foreach-Object {
+        $fileName = [System.IO.Path]::GetFileNameWithoutExtension($_.Name)
+        $newFileName = SerializeString $fileName $extension
+        $_ | Rename-Item -NewName $newFileName
+    }
+    Write-Host -ForegroundColor Green "Operation completed"
+}
+
+# end
+Write-Host ""
+function pause{ $null = Read-Host 'Press Enter to continue...' }
+pause
